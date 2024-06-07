d2aeeba1eb6cd1d92491f4ebb1e1aa3dec9b6f7e
Vladimir Pristansky <pristanskyvs@mail.ru>
2024-06-07 13:41:11 +0300

    Реализация методов класса (обработки данных и получения данных)

--- materials/models.py
+++ materials/models.py
@@ -4,6 +4,10 @@ class Material(models.Model):
     title = models.CharField(max_length=150, verbose_name='Название')
     body = models.TextField(verbose_name='Содержимое')
 
+    views_count = models.IntegerField(default=0, verbose_name='просмотры')
+    is_published = models.BooleanField(default=True, verbose_name='опубликовано')
+    slug = models.CharField(max_length=150, verbose_name='slug', null=True, blank=True)
+
     def __str__(self):
         return self.title
 
