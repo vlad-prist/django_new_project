1712eb90b4c2d2cbf224949450438ef930780b47
Vladimir Pristansky <pristanskyvs@mail.ru>
2024-06-30 15:00:02 +0300

    Создание Аутентификации

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'new_app',
     'materials',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -142,3 +143,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
+
+AUTH_USER_MODEL = 'users.User'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
--- config/urls.py
+++ config/urls.py
@@ -22,5 +22,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('new_app.urls', namespace='new_app')),
-    path('materials/', include('materials.urls', namespace='materials'))
+    path('materials/', include('materials.urls', namespace='materials')),
+    path('users/', include('users.urls', namespace='users'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
